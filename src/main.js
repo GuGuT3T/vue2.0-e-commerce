3cd2f9f164b52bab69a3f995c5c4e3eb0ee0d59d
gyk <634220187@qq.com>
2018-03-21 07:56:21 +0800

    2018.3.21.7.56

--- src/main.js
+++ src/main.js
@@ -3,13 +3,50 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
 Vue.config.productionTip = false
+    // 组件注册
+Vue.component('my-header', {
+        template: '<p>{{fruit}} 我是头部</p>',
+        data: function() {
+            return {
+                fruit: '水果1'
+            }
+        }
+    })
+    // let myHeader2Child = {
+    //     template: '<p> 我是头部2子 </p>',
+    // }
+let myHeader2 = {
+    template: '<p>{{fruit}} 我是头部2</p>',
+    // components: {
+    //     'my-header2-child': myHeader2Child
+    // },
+    data: function() {
+        return {
+            fruit: '水果2'
+        }
+    }
+}
 
-/* eslint-disable no-new */
+// new Vue({
+//         el: '#app',
+//         router,
+//         template: '<div>{{ fruit }}</div>',
+//         data: {
+//             fruit: 'apple'
+//         }
+//     })
+// new Vue({ // 组件与组件之间的挂载
+//     el: 'body',
+//     router,
+//     components: { App }
+// })
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    data: {
+        word: 'hello word2'
+    },
+    components: {
+        'my-header2': myHeader2
+    }
 })
