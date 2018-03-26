25562354441d1d23216709ce36e746883d53224e
gyk <634220187@qq.com>
2018-03-27 07:55:16 +0800

    2018.3.27

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
+<!-- <componentA></componentA> -->
   <div id="app">
     {{ hello }}
     <p v-html='hello'></p>
     {{ num + 1}}
     {{ status ? 'success' : 'fail'  }}
-    <li v-for = "(item,index) in list" :class="{add:index % 2}">
+    <ul>
+      <li v-for = "(item,index) in list" :class="{add:index % 2}">
       {{ item.name }} - {{ item.price }} - {{ index }}
-    </li>
+      </li>
+    </ul>
+    <ul>
+        <li v-for = "(value,key) in oblist">{{value}}+{{key}}</li>
+    </ul>
+    <button v-on:click="addItem">添加</button>
   </div>
 </template>
 
 <script>
+// import componentA from './components/a'
 export default {
+  // components: { // ES6
+  //   componentA
+  //   },
+   // components: { // ES5
+  //   componentA ：componentA
+  //   },
   name: 'App',
   data () {
     return {
@@ -27,8 +41,20 @@ export default {
           name:'apple2',
           price: 36
         },
-      ]
+      ],
+      oblist: {
+        name: 'apple',
+        price: 34,
+      },
     }
+  },
+  methods: {
+    // addItem: function(){ //ES5
+
+    // },
+    addItem (){
+          console.log(this.list)
+        }
   }
 }
 </script>
